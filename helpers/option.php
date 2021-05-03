b3ba6665eb6988e97a82825ec6f4af102d9a4a7e
M@hd! <mahdi75akbari@gmail.com>
2021-05-03 11:29:26 +0430

    Add getOption method

--- helpers/option.php
+++ helpers/option.php
@@ -14,3 +14,16 @@ if ( !function_exists('option') ) {
         return app('cafesource.option');
     }
 }
+
+if( !function_exists('getOption') ) {
+    /**
+     * @param string|int $key
+     * @param $default 
+     * 
+     * @return mixed
+     */
+    function getOption($key, $default = null)
+    {
+        return option()->first($key, $default);
+    }
+}
