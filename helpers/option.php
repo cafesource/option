1fccb8907cfca2768dd9078c4ec1ad6b30badccf
M@hd! <mahdi75akbari@gmail.com>
2021-05-10 09:58:49 +0430

    Remove getOption method

--- helpers/option.php
+++ helpers/option.php
@@ -5,25 +5,10 @@ use Cafesource\Option\Facades\Option;
 
 if ( !function_exists('option') ) {
     /**
-     * @param $key
-     *
      * @return Option
      */
-    function option()
+    function option() : Option
     {
         return app('cafesource.option');
-    }
-}
-
-if( !function_exists('getOption') ) {
-    /**
-     * @param string|int $key
-     * @param $default 
-     * 
-     * @return mixed
-     */
-    function getOption($key, $default = null)
-    {
-        return option()->first($key, $default);
     }
 }
