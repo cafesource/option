259b1bcd8b1555659660c3f2ea1acd196533e781
Mahdi akbari <mahdi75akbari@gmail.com>
2021-12-26 09:19:30 +0330

    Update Option.php

--- src/Facades/Option.php
+++ src/Facades/Option.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Facade;
  *
  * @method static bool has(string $key)
  * @method static \Cafesource\Option\Models\Option first(string $key, $default = null)
- * @method static mixed get(mixed $key)
+ * @method static mixed get(array|string $key)
  * @method static mixed add(string $key, mixed $value = null, string $option = null)
  * @method static mixed update(string $key, mixed $value = null, string $option = null)
  * @method static mixed updateOrAdd(string $key, mixed $value = null, string $option = null)
